ce1ec77edfb28da385b65271f01d6e019ef966a6
richCode <chineduogada@gmail.com>
2020-02-16 16:04:55 +0100

    add textarea and post cta

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <button>post</button>
+    </div>
+  );
+}
