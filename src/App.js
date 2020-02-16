6db3ca71508ab515d69749b232317a881c43b57b
richCode <chineduogada@gmail.com>
2020-02-16 15:59:44 +0100

    remove counter named import and hooks

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Counter from "./components/Counter/Counter";
+import React from "react";
 
 function App() {
-  const [state, setState] = useState({ count: 5, error: null });
-
-  const handleAdd = newCount => {
-    if (newCount < 9) {
-      newCount += 1;
-      setState(state => ({ ...state, count: newCount }));
-    } else {
-      setState(state => ({ ...state, error: "Should be less than 10" }));
-    }
-  };
-
-  const handleMinus = newCount => {
-    if (newCount) {
-      newCount -= 1;
-      setState(state => ({ ...state, count: newCount }));
-    } else {
-      setState(state => ({ ...state, error: "Should be truthy" }));
-    }
-  };
-
-  const handleDismissError = () => {
-    setState(state => ({ ...state, error: null }));
-  };
-
-  useEffect(() => {
-    handleDismissError();
-  }, [state.count]);
-
   return (
     <div style={{ display: "grid", placeItems: "center", minHeight: "100vh" }}>
-      <Counter
-        state={state}
-        onAdd={handleAdd}
-        onMinus={handleMinus}
-        onDismissError={handleDismissError}
-      />
+      develop
     </div>
   );
 }
