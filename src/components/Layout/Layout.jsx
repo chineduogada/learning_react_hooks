6869a8f5013db26cd633eb30e9a67688a628ea2e
richCode <chineduogada@gmail.com>
2020-02-21 16:09:26 +0100

    extract brand into a component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Layout.scss";
 import Navbar from "../Navbar/Navbar";
+import Brand from "./../Brand/Brand";
 
 export default function Layout() {
   return (
     <div className="layout">
       <aside className="layout__aside">
-        <div className="layout__brand">MY E-SHOP</div>
+        <div className="layout__brand">
+          <Brand />
+        </div>
       </aside>
 
       <main className="layout__main-area">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Navbar.scss";
 import { AiFillPlusCircle } from "react-icons/ai";
+import Brand from "./../Brand/Brand";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar__list">
-        <li className="navbar__item">MY E-SHOP</li>
+        <li className="navbar__item">
+          <Brand />
+        </li>
 
         <li className="navbar__item">
           <span className="avatar"></span>
