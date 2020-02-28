ed47ab60b5594e7ad1cadbcbb532e26c2dd8b653
richCode <chineduogada@gmail.com>
2020-02-28 05:54:37 +0100

    create product component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Layout.scss";
 import Navbar from "../Navbar/Navbar";
 import Aside from "../Aside/Aside";
+import Product from "../Product/Product";
 
 export default function Layout() {
   return (
@@ -11,7 +12,9 @@ export default function Layout() {
       <main className="layout__main-area">
         <Navbar />
 
-        <section className="layout__main-area__section"></section>
+        <section className="layout__main-area__section">
+          <Product />
+        </section>
       </main>
     </div>
   );
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div className="product">
+      <div className="product__img-wrapper"></div>
+      <h3 className="product__title">the title</h3>
+      <p className="product__body">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+        delectus inventore deleniti itaque libero. Nostrum?
+      </p>
+    </div>
+  );
+}
