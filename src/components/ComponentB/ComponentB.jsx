19aa1e8e8e6ef58c2c28fa4033e5f55531b17edf
richCode <chineduogada@gmail.com>
2020-03-03 11:30:18 +0100

    useContext hook

--- src/components/ComponentB/ComponentB.jsx
+++ src/components/ComponentB/ComponentB.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import ComponentC from "../ComponentC/ComponentC";
+import { userContext, statusContext } from "../../App";
 
 function ComponentB() {
+  const user = useContext(userContext);
+  const status = useContext(statusContext);
+
   return (
     <div>
       <h1>Component B</h1>
-
+      {user} - {status ? "online" : "offline"}
       <ComponentC />
     </div>
   );
